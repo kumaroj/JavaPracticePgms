bf314144e852018987dcc5bf655c1de04b22f8f7
kumaroj <manishkr2207@gmail.com>
2024-02-07 01:22:42 +0530

    Added the changes

--- src/main/java/tct1/CharacterFreqString.java
+++ src/main/java/tct1/CharacterFreqString.java
@@ -2,7 +2,7 @@ package tct1;
 
 public class CharacterFreqString {
     public static void main(String[] args) {
-
+        String s = "aaabbcccsssddd";
     }
 
     public static String characterFreq(String s){
